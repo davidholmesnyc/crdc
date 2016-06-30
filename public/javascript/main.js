9119ad459597a4ea90f8f965adff863655db6a3d
David Holmes <davidholmesnyc@gmail.com>
2016-06-29 23:58:09 -0400

    fixes

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -260,7 +260,7 @@ $(function(){
   $('.button-collapse').sideNav({
         menuWidth: 300, // Default is 240
         edge: 'left', // Choose the horizontal origin
-        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        closeOnClick: false // Closes side-nav on <a> clicks, useful for Angular/Meteor
       }
     );
 
